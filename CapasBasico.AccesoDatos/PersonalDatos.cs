fb0b4be26202b7ddf9a374ac791e621de8013a56
AngelCerna <jcerna@morningvalue.com>
2022-10-13 11:37:47 -0500

    add class PersonalDatos and PersonalLogica

---
+++ CapasBasico.AccesoDatos/PersonalDatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapasBasico.Entidades;
+
+namespace CapasBasico.AccesoDatos
+{
+    public class PersonalDatos
+    {
+
+    }
+}
