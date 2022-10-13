89a518f8514b105f96afe044a178790443b21ae7
AngelCerna <jcerna@morningvalue.com>
2022-10-13 15:53:13 -0500

    add GetAll Personal

--- CapasBasico.LogicaNegocio/PersonalLogica.cs
+++ CapasBasico.LogicaNegocio/PersonalLogica.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CapasBasico.AccesoDatos.Interface;
+using CapasBasico.Entidades;
+using CapasBasico.LogicaNegocio.Interface;
 
 namespace CapasBasico.LogicaNegocio
 {
-    public class PersonalLogica
+    public class PersonalLogica : IPersonalLogica
     {
+        private readonly IPersonalDatos _personalDatos;
+
+        public PersonalLogica(IPersonalDatos personalDatos) 
+        { 
+            _personalDatos = personalDatos;
+        }
+
+        public IEnumerable<Personal> GetAllPersonal()
+        {
+            return _personalDatos.GetAll();
+        }
     }
 }
